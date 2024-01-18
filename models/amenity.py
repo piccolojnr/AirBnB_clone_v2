1d52bee2895fa193c1a1bae0bca2aec45f7c814c
piccolojnr <rahimdaud246@gmail.com>
2024-01-18 06:31:00 +0000

    place_amenity

--- models/amenity.py
+++ models/amenity.py
@@ -4,13 +4,14 @@ from models.base_model import BaseModel, Base
 from sqlalchemy import String, Column
 from sqlalchemy.orm import relationship
 from os import getenv
-from models.place import place_amenity
 
 HBNB_TYPE_STORAGE = getenv("HBNB_TYPE_STORAGE")
 
 
 class Amenity(BaseModel, Base):
     if HBNB_TYPE_STORAGE == "db":
+        from models.place import place_amenity
+
         __tablename__ = "amenities"
         name = Column(String(128), nullable=False)
         place_amenities = relationship(
